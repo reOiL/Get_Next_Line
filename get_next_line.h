40660ab745f4e6716c90b8ccd99c64ccebcef9d3
Johnny Webber <jwebber@vo-i4.21-school.ru>
2019-09-24 17:38:18 +0300

    Worked version with list

--- get_next_line.h
+++ get_next_line.h
@@ -15,7 +15,15 @@
 
 # include "libft/libft.h"
 # include <unistd.h>
-# define BUFF_SIZE 45
+# define BUFF_SIZE 9999
+
+typedef struct s_file	t_file;
+
+struct	s_file
+{
+	int		fd;
+	char	*fdata;
+};
 
 int		get_next_line(const int fd, char **line);
 
